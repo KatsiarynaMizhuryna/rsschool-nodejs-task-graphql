974044d7f15286a699956354bd262903d4f07268
Kate <ekaterina21051988@yandex.by>
2024-03-01 20:12:23 +0200

    feat: Schema has been added

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,6 @@
 import { Type } from '@fastify/type-provider-typebox';
+import { GraphQLSchema } from 'graphql';
+import { Query } from './models/Query.js';
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -18,3 +20,7 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+export const Schema = new GraphQLSchema({
+  query: Query,
+});
