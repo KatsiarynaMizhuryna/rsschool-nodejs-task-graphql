b07ee647bdf3e461925319e8ad34d5faccbf571c
Kate <ekaterina21051988@yandex.by>
2024-03-04 17:05:21 +0200

    feat: Profile logic has been updated

--- src/routes/graphql/models/Profile.ts
+++ src/routes/graphql/models/Profile.ts
@@ -1,5 +1,9 @@
 import { GraphQLBoolean, GraphQLInt, GraphQLObjectType, GraphQLString } from 'graphql';
 import { UUIDType } from '../types/uuid.js';
+import { MemberType } from './Member.js';
+import { PrismaClient } from '@prisma/client/index.js';
+
+export type Context = { prisma: PrismaClient };
 
 export const ProfileType = new GraphQLObjectType({
   name: 'Profile',
@@ -9,5 +13,13 @@ export const ProfileType = new GraphQLObjectType({
     yearOfBirth: { type: GraphQLInt },
     userId: { type: UUIDType },
     memberTypeId: { type: GraphQLString },
+    memberType: {
+      type: MemberType,
+      resolve: async (parent: { memberTypeId: string }, _args, context: Context) => {
+        return await context.prisma.memberType.findUnique({
+          where: { id: parent.memberTypeId },
+        });
+      },
+    },
   }),
 });
